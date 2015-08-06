7ac1f9747bc37ec69a721031d135fdaa20fa6733
Hunter Skrasek <hunterskrasek@me.com>
2015-08-06 12:53:55 -0500

    Reference the correct page to list gamers

--- app/Http/Controllers/GamersController.php
+++ app/Http/Controllers/GamersController.php
@@ -16,7 +16,7 @@ class GamersController extends Controller
     {
         $gamers = Gamer::with('games')->get();
 
-        return view('pages.gamer', compact('gamers'));
+        return view('pages.gamerS', compact('gamers'));
     }
 
     /**
